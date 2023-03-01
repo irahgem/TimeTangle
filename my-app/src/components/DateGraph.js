182ddee81c3b0a0b982e15d162373b9f816d33bb
Harish <harishbharish6@gmail.com>
2023-03-01 23:08:55 +0530

    Added DateGraph.js

---
+++ my-app/src/components/DateGraph.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+function DateGraph({ data, setSelectedDate }) {
+  const [chartData, setChartData] = useState(null);
+
+  // aggregate the data by date
+  const dates = {};
+  data.forEach((schedule) => {
+    const date = schedule.item_date;
+    if (!dates[date]) {
+      dates[date] = 0;
+    }
+    dates[date]++;
+  });
+
+  // convert the dates object to an array for chart.js
+  const chartLabels = Object.keys(dates);
+  const chartValues = Object.values(dates);
+
+  // create the chart data object
+  const chartDataObject = {
+    labels: chartLabels,
+    datasets: [
+      {
+        label: 'Scheduled Meals',
+        data: chartValues,
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+      },
+    ],
+  };
+
+  const handleDateClick = (event, chartElements) => {
+    if (chartElements.length > 0) {
+      const selectedDate = chartElements[0].label;
+      setSelectedDate(selectedDate);
+    }
+  };
+
+  return (
+    <div>
+      {chartDataObject && (
+        <Bar
+          data={chartDataObject}
+          options={{
+            onClick: handleDateClick,
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default DateGraph;
